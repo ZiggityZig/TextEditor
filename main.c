e5faf00b4713907ed5e803522d085734604a8505
ZiggityZig <zigi531@gmail.com>
2021-06-10 17:13:26 +0300

    Turned off more terminal flags,  error handling

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -8,18 +9,35 @@
 typedef struct termios termios;
 
 termios orig_termios;
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disableRawMode()
 {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode()
 {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    die("tcgetattr");
   atexit(disableRawMode);
+
   termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON| IEXTEN| ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    die("tcsetattr");
 }
 
 int main(int argc, char **argv)
@@ -33,27 +51,31 @@ int main(int argc, char **argv)
     if (strncmp(argv[i], "-D", 2) == 0)
     {
       debug = 1;
-      fprintf(stderr, "Running in debug mode\n");
+      fprintf(stderr, "Running in debug mode\r\n");
     }
   }
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    {
+    while (1) {
+      c = '\0';
+      if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+        die("read");
+      read(STDIN_FILENO, &c, 1);
       if (iscntrl(c))
       {
         if(debug)
-          printf("%d\n", c);
+          printf("%d\r\n", c);
       }
       else
       {
-        if(debug)
-          printf("%d ('%c')\n", c, c);
+        if(debug)     
+          printf("%d ('%c')\r\n", c, c);
         else {
            printf("%c", c);
           fflush(stdout);
         }
          
       }
+      if (c == 'q') 
+        break;
     }
-    disableRawMode();
     return 0;
   }
