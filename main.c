a3ac85b374bea098b84b226989c4291e7c0a19c8
ZiggityZig <zigi531@gmail.com>
2021-06-10 16:05:05 +0300

    Implemented Raw Mode

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
+#include <termios.h>
 #include <unistd.h>
 
+
+void enableRawMode() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main (int argc, char** argv){
+    enableRawMode();
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        printf("%c", c);
+    }
 
     return 0;
 }
